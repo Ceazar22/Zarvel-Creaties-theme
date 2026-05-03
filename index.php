b0c1c08879a312bd161e2d093ead98b9caaab6d5
Cyvior <bryanceazartabanas@gmail.com>
2026-05-03 23:41:25 +0800

    Test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main class="site-main">
-  <h1>Index Fallback Page</h1>  test
+  <h1>Index Fallback Page</h1>
   <p>WordPress is using index.php, not front-page.php.</p>
 </main>
 
