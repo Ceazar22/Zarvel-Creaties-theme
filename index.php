61fb95136115fb642c09c146ee525f089a2f066c
Bryan Ceazar Tabanas <bryanceazartabanas@gmail.com>
2026-05-03 23:54:05 +0800

    Test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main class="site-main">
+<main class="site-main"> Bryan
   <h1>Index Fallback Page</h1>
   <p>WordPress is using index.php, not front-page.php.</p>
 </main>
